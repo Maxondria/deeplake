6edde9025b7abaebafb5b1b7bd27d207c8a7d62d
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-07-14 00:04:43 +0300

    understanding memory

---
+++ tchat/main.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.llm import LLMChain
+from langchain.memory import ConversationBufferMemory
+
+load_dotenv()
+
+prompt = ChatPromptTemplate(
+    input_variables=["content", "chat_history"],
+    messages=[
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+
+
+llm = ChatOpenAI()
+
+memory = ConversationBufferMemory(
+    memory_key="chat_history", return_messages=True
+)
+
+chain = LLMChain(llm=llm, prompt=prompt, memory=memory)
+
+
+while True:
+    content = input(">> ")
+    if len(content) > 0:
+        result = chain.invoke({"content": content})
+        print(result["text"])
